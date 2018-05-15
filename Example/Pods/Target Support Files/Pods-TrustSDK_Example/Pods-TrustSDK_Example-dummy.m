d1acb1d63a53ce3dc90175acbe04375d74b32791
Alejandro Isaza <al@isaza.ca>
2018-05-15 10:54:37 -0700

    Update exmaple project

--- Example/Pods/Target Support Files/Pods-TrustSDK_Example/Pods-TrustSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TrustSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_TrustSDK_Example
-@end
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -1,34 +1,58 @@
+// Copyright © 2018 Trust.
 //
-//  ViewController.swift
-//  TrustSDK
-//
-//  Created by Viktor Radchenko on 05/09/2018.
-//  Copyright (c) 2018 Viktor Radchenko. All rights reserved.
-//
+// This file is part of TrustSDK. The full TrustSDK copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
 
-import UIKit
+import BigInt
+import TrustCore
 import TrustSDK
+import UIKit
 
 class ViewController: UIViewController {
+    var trustSDK: TrustSDK!
 
-    let trust = TrustSDK()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet private weak var addressTextField: UITextField!
+    @IBOutlet private weak var amountTextField: UITextField!
+    @IBOutlet private weak var messageTextField: UITextField!
 
     @IBAction func signTransaction(_ sender: Any) {
-        trust.signTransaction(in: self)
+        guard let address = Address(eip55: addressTextField.text!) else {
+            let alert = UIAlertController(title: "Invalid Address", message: nil, preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+
+        guard let amount = BigInt(amountTextField.text!) else {
+            let alert = UIAlertController(title: "Invalid Amount", message: nil, preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+
+        var transaction = Transaction(gasPrice: BigInt(21), gasLimit: 21000, to: address)
+        transaction.amount = amount
+
+        trustSDK.signTransaction(transaction) { [weak self] signedTransaction in
+            let alert = UIAlertController(title: "Signed Transaction", message: nil, preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default, handler: nil))
+            self?.present(alert, animated: true, completion: nil)
+        }
     }
 
     @IBAction func signMessage(_ sender: Any) {
-        trust.signMessage(in: self)
+        guard let message = Data(hexString: messageTextField.text!) else {
+            let alert = UIAlertController(title: "Invalid Message", message: nil, preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+
+        trustSDK.signMessage(message) { [weak self] signed in
+            let alert = UIAlertController(title: "Signed Message", message: signed.hexString, preferredStyle: .alert)
+            self?.present(alert, animated: true, completion: nil)
+        }
     }
 }
 
