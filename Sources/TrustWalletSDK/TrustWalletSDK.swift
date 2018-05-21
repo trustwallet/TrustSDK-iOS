c29e209174e202783e3cc5914aa7d887cf7e2402
Alejandro Isaza <al@isaza.ca>
2018-05-20 23:31:01 -0700

    Rename payload to data

--- Sources/TrustWalletSDK/TrustWalletSDK.swift
+++ Sources/TrustWalletSDK/TrustWalletSDK.swift
@@ -89,7 +89,7 @@ public final class TrustWalletSDK {
 
         var transaction = Transaction(gasPrice: gasPrice, gasLimit: gasLimit, to: to)
         transaction.amount = amount
-        transaction.payload = components.queryParameterValue(for: "payload").flatMap({ Data(hexString: $0) })
+        transaction.payload = components.queryParameterValue(for: "data").flatMap({ Data(hexString: $0) })
         transaction.nonce = components.queryParameterValue(for: "nonce").flatMap({ UInt64($0) }) ?? 0
 
         delegate.signTransaction(transaction) { signedTransaction in
