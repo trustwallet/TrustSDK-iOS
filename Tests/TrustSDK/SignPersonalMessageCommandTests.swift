649428ee4ccbe3ea6cd31f7fa6cf0dc90e6da770
Viktor Radchenko <yazexel@gmail.com>
2018-05-29 02:29:48 -0700

    Implement SignPersonalMessageCommand and add tests

---
+++ Tests/TrustSDK/SignPersonalMessageCommandTests.swift
@@ -0,0 +1,36 @@
+// Copyright © 2018 Trust.
+//
+// This file is part of TrustSDK. The full TrustSDK copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+@testable import TrustSDK
+import XCTest
+
+class SignPersonalMessageCommandTests: XCTestCase {
+
+    func testRequestURL() {
+        let message = Data(hexString: "1234")!
+        let command = SignPersonalMessageCommand(message: message, address: nil, callbackScheme: "app") { _ in
+            // Ignore
+        }
+        let url = command.requestURL(scheme: "trust")
+
+        XCTAssertEqual(url.absoluteString, "trust://sign-personal-message?message=EjQ%3D&callback=app://sign-personal-message")
+    }
+
+    func testHandleCallback() {
+        let message = Data(hexString: "1234")!
+        let commandCompletedExpectation = expectation(description: "Command completed")
+        let command = SignPersonalMessageCommand(message: message, address: nil, callbackScheme: "app") { data in
+            commandCompletedExpectation.fulfill()
+            XCTAssertEqual(data, message)
+        }
+
+        let url = URL(string: "app://sign-personal-message?result=EjQ%3D")!
+        let handled = command.handleCallback(url: url)
+        XCTAssertTrue(handled)
+
+        wait(for: [commandCompletedExpectation], timeout: 0.1)
+    }
+}
