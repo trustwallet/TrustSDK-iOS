62d8c27c78284353d22250bb472e0be3dbe4d5d8
Leone Parise <1442927+leoneparise@users.noreply.github.com>
2020-03-05 13:55:20 -0300

    Update headers

--- TrustSDK/Classes/Client/Signer/Signers.swift
+++ TrustSDK/Classes/Client/Signer/Signers.swift
@@ -1,4 +1,4 @@
-// Copyright DApps Platform Inc. All rights reserved.
+// Copyright Trust Wallet. All rights reserved.
 //
 // This file is part of TrustSDK. The full TrustSDK copyright notice, including
 // terms governing use, modification, and redistribution, is contained in the
